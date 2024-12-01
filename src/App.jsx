e96449bf8bb530b8821a6f4d247c7e0b0acd58eb
mjfardani <muhjauharfardani@gmail.com>
2024-12-01 07:40:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return (
+    <div>
+      <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+        <h1>Biodata Perusahaan</h1>
+      </a>
+      <ul>
+        <li>Nama: Dicoding Indonesia</li>
+        <li>Bidang: Education</li>
+        <li>Tagline: Decode Ideas, Discover Potential.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
